e829e8f36437c7488107d0117be3ef39a87f9725
Paulo Viadanna <paulo@viadanna.net>
2022-05-30 08:18:20 +0000

    Refactor experiment.py

--- experiment.py
+++ experiment.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-docstring,invalid-name,line-too-long
+# pylint: disable=missing-docstring,invalid-name,line-too-long, bare-except
 import argparse
 import json
 
@@ -20,6 +20,68 @@ model_map = {
     'inception': inception_pretrained,
 }
 
+def train(args):
+    # Train given model
+    mm = model_map[args.model]()
+    # Callbacks for early stopping and tensorboard logging
+    callbacks = [
+        EarlyStopping(monitor='val_loss', patience=args.patience, verbose=1),
+        TensorBoard(f'output/{get_model_name(args)}_logs', write_graph=True),
+        ModelCheckpoint(filepath=get_model_path(args), save_best_only=True),
+    ]
+
+    if args.augment <= 120000:
+        # Couldn't save a pickle bigger than that
+        X_train, y_train, X_valid, y_valid, _, _ = get_datasets(args.augment)
+        mm.fit(
+            x=X_train,
+            y=y_train,
+            batch_size=args.batch,
+            epochs=args.epochs,
+            validation_data=(X_valid, y_valid),
+            callbacks=callbacks)
+    else:
+        # Generate agumented dataset as the model is trained
+        X_train, y_train, X_valid, y_valid, _, _ = get_datasets()
+        mm.fit_generator(
+            generator=datagen.flow(X_train, y_train, batch_size=args.batch),
+            steps_per_epoch=args.augment // args.batch,
+            epochs=args.epochs,
+            validation_data=(X_valid, y_valid),
+            callbacks=callbacks)
+
+    # Reload best model
+    return load_model(get_model_path(args))
+
+
+def evaluate(model, args):
+    """ Evaluates the test dataset """
+    X_test, y_test = get_datasets(args.augment)[4:]
+    test_loss, test_accuracy = model.evaluate(X_test, y_test)
+    prediction = model.predict(X_test)
+    return {
+        'test_accuracy': test_accuracy,
+        'test_loss': test_loss,
+        'y_true': y_test.tolist(),
+        'y_pred': prediction.tolist(),
+    }
+
+
+def get_model_name(args):
+    return f'{args.model}_{args.augment}'
+
+
+def get_model_path(args):
+    return f'output/{get_model_name(args)}.h5'
+
+
+def get_model(args):
+    try:
+        return load_model(get_model_path(args))
+    except:
+        return train(args)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('CIFAR-10 experiments')
     parser.add_argument('model', help='Which model(s) to run')
@@ -27,58 +89,10 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--batch', type=int, default=128, help='Batch size.')
     parser.add_argument('-e', '--epochs', type=int, default=1000, help='Number of epochs.')
     parser.add_argument('-p', '--patience', type=int, default=5, help='Early Stopping patience.')
-    args = parser.parse_args()
+    arguments = parser.parse_args()
 
-    model_name = '{}_{}'.format(args.model, str(args.augment))
-    checkpoint_path = 'output/{}.h5'.format(model_name)
-    logs_path = 'output/{}_logs'.format(model_name)
-
-    try:
-        # Tries to used saved model
-        model = load_model(checkpoint_path)
-        _, _, _, _, X_test, y_test = get_datasets(args.augment)
-    except:
-        # Train given model
-        model = model_map[args.model]()
-        # Callbacks for early stopping and tensorboard logging
-        callbacks = [
-            EarlyStopping(monitor='val_loss', patience=args.patience, verbose=1),
-            TensorBoard(logs_path, write_graph=True),
-            ModelCheckpoint(filepath=checkpoint_path, save_best_only=True),
-        ]
-
-        if args.augment <= 120000:
-            # Couldn't save a pickle bigger than that
-            X_train, y_train, X_valid, y_valid, X_test, y_test = get_datasets(args.augment)
-            history = model.fit(
-                x=X_train,
-                y=y_train,
-                batch_size=args.batch,
-                epochs=args.epochs,
-                validation_data=(X_valid, y_valid),
-                callbacks=callbacks)
-        else:
-            # Generate agumented dataset as the model is trained
-            X_train, y_train, X_valid, y_valid, X_test, y_test = get_datasets()
-            history = model.fit_generator(
-                generator=datagen.flow(X_train, y_train, batch_size=args.batch),
-                steps_per_epoch=args.augment // args.batch,
-                epochs=args.epochs,
-                validation_data=(X_valid, y_valid),
-                callbacks=callbacks)
-
-        # Reload best model
-        model = load_model(checkpoint_path)
-    finally:
-        # Evaluate test dataset
-        test_loss, test_accuracy = model.evaluate(X_test, y_test)
-        prediction = model.predict(X_test)
-
-        # Save results
-        with open('output/{}.json'.format(model_name), 'w') as f:
-            json.dump({
-                'test_accuracy': test_accuracy,
-                'test_loss': test_loss,
-                'y_true': y_test.tolist(),
-                'y_pred': prediction.tolist(),
-            }, f)
+    model = get_model(arguments)
+    results = evaluate(model, arguments)
+    # Save results
+    with open(f'output/{get_model_name(arguments)}.json', 'w', encoding='utf-8') as f:
+        json.dump(results, f)
